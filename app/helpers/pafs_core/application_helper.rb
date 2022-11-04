524450ba092ab8bf585234861a62d82184e15c9b
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-11-04 13:50:21 +0000

    Update page title construction logic

--- app/helpers/pafs_core/application_helper.rb
+++ app/helpers/pafs_core/application_helper.rb
@@ -12,8 +12,20 @@ module PafsCore
                   class: "pafs_form")
     end
 
-    def make_page_title(title)
-      "#{title} - #{t(:global_proposition_header)} - GOV.UK"
+    # wrap the formbuilder method here in order to set content for page title
+    def form_error_header(form, heading = nil, description = nil, sort_order = nil)
+      error_header = form.error_header(heading, description, sort_order)
+      return if error_header.blank?
+
+      content_for :error_title, "Error", flush: true
+      error_header
+    end
+
+    def title
+      title_elements = [error_title, title_text, t(:global_proposition_header), "GOV.UK"]
+      # Remove empty elements, for example if no specific title is set
+      title_elements.compact_blank!
+      title_elements.join(" - ")
     end
 
     # we're not including Devise in the engine so the current_user
@@ -83,5 +95,25 @@ module PafsCore
       end
       { next_sort_order: next_sort_order, curr_arrow: arrow }
     end
+
+    private
+
+    def title_text
+      # Check if the title is set in the view (we do this for High Voltage pages)
+      return content_for :title if content_for?(:title)
+
+      # Otherwise, look up translation key based on controller path, action name and .title
+      # Solution from https://coderwall.com/p/a1pj7w/rails-page-titles-with-the-right-amount-of-magic
+      title = t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: "")
+      return title if title.present?
+
+      # Default to title for "new" action if the current action doesn't return anything
+      t("#{controller_path.tr('/', '.')}.new.title", default: "")
+    end
+
+    def error_title
+      return content_for :error_title if content_for?(:error_title)
+    end
+
   end
 end
