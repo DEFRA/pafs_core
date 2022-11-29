e85b33b48ae448664b1a52fb467b76a067f23de5
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-11-29 15:50:15 +0000

    Table layout and content changes (#494)
    https://eaflood.atlassian.net/browse/RUBY-2178

--- app/models/pafs_core/state.rb
+++ app/models/pafs_core/state.rb
@@ -7,7 +7,7 @@ module PafsCore
     belongs_to :project, inverse_of: :state, optional: true
     validates :state, inclusion: { in: VALID_STATES }
 
-    def self.govuk_submitted
+    def self.submitted
       where(state: "submitted")
     end
 
