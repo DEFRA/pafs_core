c8faaa05863d0b9a61d1f705b0df213c98390a1d
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-12-19 14:39:52 +0000

    Javscript lint fix

--- app/assets/javascripts/pafs_core/other_flood_measures.js
+++ app/assets/javascripts/pafs_core/other_flood_measures.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-  let other_measures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
+  const other_measures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
   if (other_measures != null && other_measures.checked == false) {
     document.getElementById("other-flood-measures").style.visibility = "hidden";
   }
 };
 
 function toggleBoxVisibility() {
-  let other_measures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
+  const other_measures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
   if (other_measures != null && other_measures.checked == true) {
     document.getElementById("other-flood-measures").style.visibility = "visible";
   }
