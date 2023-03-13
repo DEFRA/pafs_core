4e719c1d9c39ec1c928900e2206f81ced7557efe
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-03-13 09:51:33 +0000

    Add missing spreadsheet presenter methods (#557)
    https://eaflood.atlassian.net/browse/RUBY-2166

--- spec/presenters/pafs_core/spreadsheet_presenter_spec.rb
+++ spec/presenters/pafs_core/spreadsheet_presenter_spec.rb
@@ -3,14 +3,16 @@
 require "rails_helper"
 
 RSpec.describe PafsCore::SpreadsheetPresenter do
-  subject { described_class.new(project) }
+  subject(:presenter) { described_class.new(project) }
 
-  describe "moderation_code" do
+  let(:project) { build(:project) }
+
+  describe "#moderation_code" do
     context "with an urgent project" do
       let(:project) { build(:project, urgency_reason: :legal_need) }
 
       it "returns the correct moderation code" do
-        expect(subject.moderation_code).to eql("Legal Agreement")
+        expect(presenter.moderation_code).to eql("Legal Agreement")
       end
     end
 
@@ -18,7 +20,63 @@ RSpec.describe PafsCore::SpreadsheetPresenter do
       let(:project) { build(:project, urgency_reason: :not_urgent) }
 
       it 'returns "Not Urgent"' do
-        expect(subject.moderation_code).to eql("Not Urgent")
+        expect(presenter.moderation_code).to eql("Not Urgent")
+      end
+    end
+  end
+
+  describe "#designated_site" do
+    context "when improve_spa_or_sac is set" do
+      before { project.improve_spa_or_sac = true }
+
+      it "returns 'SPA/SAC'" do
+        expect(presenter.designated_site).to eq "SPA/SAC"
+      end
+    end
+
+    context "when improve_sssi is set" do
+      before { project.improve_sssi = true }
+
+      it "returns 'SSSI'" do
+        expect(presenter.designated_site).to eq "SSSI"
+      end
+    end
+
+    context "when neither improve_spa_or_sac nor improve_sssi is set" do
+      it "returns nil" do
+        expect(presenter.designated_site).to be_nil
+      end
+    end
+  end
+
+  describe "#remove_fish_or_eel_barrier" do
+    context "when neither remove_fish_barrier nor remove_eel_barrier are set" do
+      it "returns nil" do
+        expect(presenter.remove_fish_or_eel_barrier).to be_nil
+      end
+    end
+
+    context "when remove_eel_barrier is set" do
+      before { project.remove_eel_barrier = true }
+
+      it "returns 'Eel'" do
+        expect(presenter.remove_fish_or_eel_barrier).to eq "Eel"
+      end
+    end
+
+    context "when remove_fish_barrier is set" do
+      before { project.remove_fish_barrier = true }
+
+      it "returns 'Fish'" do
+        expect(presenter.remove_fish_or_eel_barrier).to eq "Fish"
+      end
+
+      context "when remove_eel_barrier is also set" do
+        before { project.remove_eel_barrier = true }
+
+        it "returns 'Both'" do
+          expect(presenter.remove_fish_or_eel_barrier).to eq "Both"
+        end
       end
     end
   end
