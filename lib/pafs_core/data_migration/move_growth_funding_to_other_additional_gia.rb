72bb9f81b1e67fd0b6f02c53d182e284d521e4fa
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-03-30 15:23:07 +0100

    Migrate data from growth_funding to other_additional_grant_in_aid and remove that column

---
+++ lib/pafs_core/data_migration/move_growth_funding_to_other_additional_gia.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module PafsCore
+  module DataMigration
+    class MoveGrowthFundingToOtherAdditionalGia
+      class << self
+        def up
+          funding_values = PafsCore::FundingValue.where.not(growth_funding: nil, growth_funding: 0)
+          funding_values.each do |fv|
+            fv.update(other_additional_grant_in_aid: fv.growth_funding, growth_funding: nil)
+          end
+        end
+
+        def down
+          funding_values = PafsCore::FundingValue.where.not(other_additional_grant_in_aid: nil, growth_funding: 0)
+          funding_values.each do |fv|
+            fv.update(growth_funding: fv.other_additional_grant_in_aid, other_additional_grant_in_aid: nil)
+          end
+        end
+      end
+    end
+  end
+end
