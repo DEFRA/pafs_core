459be773979499c47a12fbdf09af08b533359887
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-03-31 16:37:06 +0100

    Add error to ensure no data loss, fix issue with query for funding values

--- lib/pafs_core/data_migration/move_growth_funding_to_other_additional_gia.rb
+++ lib/pafs_core/data_migration/move_growth_funding_to_other_additional_gia.rb
@@ -8,11 +8,12 @@ module PafsCore
           funding_values = PafsCore::FundingValue.where.not(growth_funding: nil, growth_funding: 0)
           funding_values.each do |fv|
             fv.update(other_additional_grant_in_aid: fv.growth_funding, growth_funding: nil)
+            raise ActiveRecord::Rollback unless fv.other_additional_grant_in_aid?
           end
         end
 
         def down
-          funding_values = PafsCore::FundingValue.where.not(other_additional_grant_in_aid: nil, growth_funding: 0)
+          funding_values = PafsCore::FundingValue.where.not(other_additional_grant_in_aid: nil, other_additional_grant_in_aid: 0)
           funding_values.each do |fv|
             fv.update(growth_funding: fv.other_additional_grant_in_aid, other_additional_grant_in_aid: nil)
           end
