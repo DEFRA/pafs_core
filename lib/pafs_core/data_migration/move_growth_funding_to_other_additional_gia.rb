0dc8b50bdd3852e3b6338ce31959ae5f74b66102
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-04-06 12:08:41 +0100

    Add other_additional_grant_in_aid boolean to migration as per new AC (#585)

--- lib/pafs_core/data_migration/move_growth_funding_to_other_additional_gia.rb
+++ lib/pafs_core/data_migration/move_growth_funding_to_other_additional_gia.rb
@@ -8,6 +8,7 @@ module PafsCore
           funding_values = PafsCore::FundingValue.where("growth_funding IS NOT NULL AND growth_funding != 0")
           funding_values.each do |fv|
             fv.update(other_additional_grant_in_aid: fv.growth_funding, growth_funding: nil)
+            fv.project.update(other_additional_grant_in_aid: true)
             raise ActiveRecord::Rollback unless fv.other_additional_grant_in_aid?
           end
         end
@@ -17,6 +18,7 @@ module PafsCore
                            .where("other_additional_grant_in_aid IS NOT NULL AND other_additional_grant_in_aid != 0")
           funding_values.each do |fv|
             fv.update(growth_funding: fv.other_additional_grant_in_aid, other_additional_grant_in_aid: nil)
+            fv.project.update(other_additional_grant_in_aid: false)
           end
         end
       end
