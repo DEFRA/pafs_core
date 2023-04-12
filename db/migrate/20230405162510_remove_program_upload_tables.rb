803599123895f0c836c7a75cc1f69ee12ae48135
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-04-12 09:55:02 +0100

    Remove program upload related code (#584)
    * Remove program upload related code
    
    * Fix migration and update schema
    
    * Fixes to spec

---
+++ db/migrate/20230405162510_remove_program_upload_tables.rb
@@ -0,0 +1,7 @@
+class RemoveProgramUploadTables < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :pafs_core_program_uploads
+    drop_table :pafs_core_program_upload_items
+    drop_table :pafs_core_program_upload_failures
+  end
+end
