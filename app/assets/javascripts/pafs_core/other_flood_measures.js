3a3510e8d6581ae16d04ca32b60569af55845d90
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-07-10 18:58:07 +0100

    SonarCloud tweaks

--- app/assets/javascripts/pafs_core/other_flood_measures.js
+++ app/assets/javascripts/pafs_core/other_flood_measures.js
@@ -1,13 +1,13 @@
 window.onload = function () {
-  const other_measures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
-  if (other_measures !== null && other_measures.checked === false) {
+  const otherMeasures = document.getElementById('natural_flood_risk_measures_step_other_flood_measures_selected');
+  if (otherMeasures !== null && otherMeasures.checked === false) {
     document.getElementById("other-flood-measures").style.visibility = "hidden";
   }
 };
 
 function toggleBoxVisibility() {
-  const other_measures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
-  if (other_measures !== null && other_measures.checked === true) {
+  const otherMeasures = document.getElementById("natural_flood_risk_measures_step_other_flood_measures_selected");
+  if (otherMeasures !== null && otherMeasures.checked === true) {
     document.getElementById("other-flood-measures").style.visibility = "visible";
   }
   else {
