b079bbff4c7477bd14cc1c5593c362a7a860ac5d
Leonid Brujev <brujev@gmail.com>
2024-03-04 22:24:11 +0000

    [RUBY-2589] adding step and submission phase validation for earliest start date field

--- app/steps/pafs_core/earliest_start_date_step.rb
+++ app/steps/pafs_core/earliest_start_date_step.rb
@@ -5,10 +5,11 @@ module PafsCore
     delegate :could_start_early?,
              :earliest_start_month, :earliest_start_month=,
              :earliest_start_year, :earliest_start_year=,
-             :date_present?, :date_plausible?, :year_plausible?, :month_plausible?,
+             :date_present?, :date_plausible?, :year_plausible?,
+             :month_plausible?, :date_in_future?,
              to: :project
 
-    validate :date_is_present_and_plausible
+    validate :date_is_present_and_correct
 
     private
 
@@ -16,6 +17,13 @@ module PafsCore
       params.require(:earliest_start_date_step).permit(
         :earliest_start_month, :earliest_start_year
       )
+    end
+
+    def date_is_present_and_correct
+      date_is_present_and_plausible
+      return if errors.any?
+
+      date_is_in_future
     end
 
     def date_is_present_and_plausible
@@ -34,6 +42,15 @@ module PafsCore
         :earliest_start_date,
         "The year must be between #{PafsCore::DateUtils::VALID_YEAR_RANGE.first} " \
         "and #{PafsCore::DateUtils::VALID_YEAR_RANGE.last}"
+      )
+    end
+
+    def date_is_in_future
+      return unless date_present?("earliest_start") && !date_in_future?("earliest_start")
+
+      errors.add(
+        :earliest_start_date,
+        "You cannot enter a date in the past"
       )
     end
   end
