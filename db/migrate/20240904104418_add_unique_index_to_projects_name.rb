a2461972f628fda704f72e63cd2601a08dd27319
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-09-04 12:05:27 +0100

    [RUBY-3212] Update migration class to inherit from ActiveRecord::Migration[7.1]
    - Changed inheritance of `AddUniqueIndexToProjectsName` from `ActiveRecord::Migration[7.2]` to `ActiveRecord::Migration[7.1]`

--- db/migrate/20240904104418_add_unique_index_to_projects_name.rb
+++ db/migrate/20240904104418_add_unique_index_to_projects_name.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddUniqueIndexToProjectsName < ActiveRecord::Migration[7.2]
+class AddUniqueIndexToProjectsName < ActiveRecord::Migration[7.1]
   def change
     add_index :pafs_core_projects, :name, unique: true
   end
