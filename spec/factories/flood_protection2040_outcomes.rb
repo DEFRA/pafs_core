6292589c63ee3f66f7a554d42fbe0c1f895bc8f0
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-12-27 17:25:11 +0000

    remove stray comment

--- spec/factories/flood_protection2040_outcomes.rb
+++ spec/factories/flood_protection2040_outcomes.rb
@@ -2,7 +2,6 @@
 
 FactoryBot.define do
   factory :flood_protection2040_outcomes, class: "PafsCore::FloodProtection2040Outcome" do
-    # project_id { 1 }
     financial_year { 2022 }
     households_at_reduced_risk { 100 }
     moved_from_very_significant_and_significant_to_moderate_or_low { 50 }
