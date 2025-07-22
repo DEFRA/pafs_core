a654ec818c0a3061b6a8b9e62f999b0690b97f7f
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-07-22 07:52:25 +0100

    [RUBY-3889] Remove bootstrap placeholders, prevent bootstrap errors in
    financial year step by ensuring date range checker is only initialized
    and used if it is an actual project, not a bootstrap project

--- app/steps/pafs_core/financial_year_step.rb
+++ app/steps/pafs_core/financial_year_step.rb
@@ -26,17 +26,19 @@ module PafsCore
 
       return false unless valid?
 
-      checker = PafsCore::DateRangeDataChecker.new(
-        project,
-        latest_date: financial_year_end_from_year(project_end_financial_year)
-      )
+      if project.is_a?(PafsCore::Project)
+        checker = PafsCore::DateRangeDataChecker.new(
+          project,
+          latest_date: financial_year_end_from_year(project_end_financial_year)
+        )
 
-      if checker.data_outside_date_range?
-        project.pending_financial_year = project_end_financial_year
-        project.date_change_requires_confirmation = true
-        # Revert the date to original until it's confirmed user wants to proceed
-        # even with data loss
-        project.project_end_financial_year = original_year
+        if checker.data_outside_date_range?
+          project.pending_financial_year = project_end_financial_year
+          project.date_change_requires_confirmation = true
+          # Revert the date to original until it's confirmed user wants to proceed
+          # even with data loss
+          project.project_end_financial_year = original_year
+        end
       end
 
       project.updated_by = user if project.respond_to?(:updated_by)
