a00960a8dbeaff0e97054cf8d9138ba7f258aae8
Leonid Brujev <brujev@gmail.com>
2025-08-01 15:39:50 +0100

    [RUBY-3926] Adding method to check if required fields are empty

--- app/steps/pafs_core/net_carbon_step.rb
+++ app/steps/pafs_core/net_carbon_step.rb
@@ -9,6 +9,13 @@ module PafsCore
       true
     end
 
+    def net_carbon_required_fields_empty?
+      project.carbon_cost_build.blank? &&
+        project.carbon_cost_operation.blank? &&
+        project.carbon_cost_sequestered.blank? &&
+        project.carbon_cost_avoided.blank?
+    end
+
     def carbon_impact_presenter
       @carbon_impact_presenter ||= PafsCore::CarbonImpactPresenter.new(project: project)
     end
