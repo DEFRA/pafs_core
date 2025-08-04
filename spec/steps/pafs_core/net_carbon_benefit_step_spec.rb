2cf20447042f3cf7670fd8ad99a862c132e4e89a
Leonid Brujev <brujev@gmail.com>
2025-08-04 12:10:46 +0100

    [RUBY-3927] improving test coverage

--- spec/steps/pafs_core/net_carbon_benefit_step_spec.rb
+++ spec/steps/pafs_core/net_carbon_benefit_step_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe PafsCore::NetCarbonBenefitStep, type: :model do
     it_behaves_like "validates numericality", :net_carbon_benefit_step, :carbon_savings_net_economic_benefit, negatives_allowed: true
   end
 
-  it_behaves_like "updates project attributes", :net_carbon_benefit_step, :carbon_savings_net_economic_benefit, negatives_allowed: true
+  describe "#update" do
+    it_behaves_like "updates project attributes", :net_carbon_benefit_step, :carbon_savings_net_economic_benefit, negatives_allowed: true
+
+    it "allows saving a negative carbon savings value" do
+      value = Faker::Number.negative.round(2)
+      params = ActionController::Parameters.new(
+        net_carbon_benefit_step: { carbon_savings_net_economic_benefit: value }
+      )
+
+      expect { subject.update(params) }.to change { subject.project.reload.carbon_savings_net_economic_benefit }.to(value)
+    end
+  end
 end
