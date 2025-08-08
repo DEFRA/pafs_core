1d501b32484559e950143b8e029ee0e45a6c70f8
Leonid Brujev <brujev@gmail.com>
2025-08-08 13:40:54 +0100

    [RUBY-3800] Refactor WholeLifeCarbonStep to use presenter method to display whole life carbon

--- spec/steps/pafs_core/whole_life_carbon_step_spec.rb
+++ spec/steps/pafs_core/whole_life_carbon_step_spec.rb
@@ -9,22 +9,9 @@ RSpec.describe PafsCore::WholeLifeCarbonStep, type: :model do
 
   it_behaves_like "does not modify project attributes"
 
-  describe "#whole_life_carbon_required_fields_empty?" do
-    it "returns true if all carbon cost fields are blank" do
-      subject.project.carbon_cost_build = nil
-      subject.project.carbon_cost_operation = nil
-      expect(subject.whole_life_carbon_required_fields_empty?).to be true
-    end
-
-    it "returns false if any carbon cost field is present" do
-      subject.project.carbon_cost_build = 100
-      expect(subject.whole_life_carbon_required_fields_empty?).to be false
-    end
-  end
-
-  describe "#carbon_impact_presenter" do
+  describe "#presenter" do
     it "returns a CarbonImpactPresenter instance" do
-      presenter = subject.carbon_impact_presenter
+      presenter = subject.presenter
       expect(presenter).to be_a(PafsCore::CarbonImpactPresenter)
     end
   end
