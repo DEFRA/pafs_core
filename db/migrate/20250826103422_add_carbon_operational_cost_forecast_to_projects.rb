9de1d32ee4f8ce3ad95c0092031e8ab450dd7882
Leonid Brujev <brujev@gmail.com>
2025-08-26 12:37:53 +0100

    [RUBY-3984] changing carbon_operational_cost_forecast to bigint

--- db/migrate/20250826103422_add_carbon_operational_cost_forecast_to_projects.rb
+++ db/migrate/20250826103422_add_carbon_operational_cost_forecast_to_projects.rb
@@ -2,6 +2,6 @@
 
 class AddCarbonOperationalCostForecastToProjects < ActiveRecord::Migration[7.2]
   def change
-    add_column :pafs_core_projects, :carbon_operational_cost_forecast, :decimal, precision: 20, scale: 2
+    add_column :pafs_core_projects, :carbon_operational_cost_forecast, :integer, limit: 8
   end
 end
