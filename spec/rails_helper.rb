e0a35ef137e1462fbb4b08563c9d83846fee0ff0
Leonid Brujev <brujev@gmail.com>
2025-11-24 11:42:49 +0000

    [RUBY-4069] Fixing sonar cloud issues - Move this 'require' statement to the top of the file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,8 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
-ENV["RAILS_ENV"] ||= "test" # NOSONAR
-require File.expand_path("dummy/config/environment", __dir__)
+require((ENV["RAILS_ENV"] ||= "test") && File.expand_path("dummy/config/environment", __dir__))
 require "rspec/rails"
 # require "capybara/rspec"
 require "factory_bot_rails"
