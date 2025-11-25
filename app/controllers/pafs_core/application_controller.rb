f9dc5916a4fd7a9d1e7c902a3e64e16b45579deb
Leonid Brujev <brujev@gmail.com>
2025-11-25 12:31:27 +0000

    [RUBY-4069] removing # nosonar comment because it's just ignored by sonar cloud

--- app/controllers/pafs_core/application_controller.rb
+++ app/controllers/pafs_core/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PafsCore
-  class ApplicationController < ActionController::Base # NOSONAR
+  class ApplicationController < ActionController::Base
     include PafsCore::ApplicationHelper
     include PafsCore::CustomHeaders
 
